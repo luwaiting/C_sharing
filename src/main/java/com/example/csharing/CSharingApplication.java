11b93e00c87d7fd8da1664616b6911cc12ca4d3c
Lu-weiting <runeeld23@gmail.com>
2023-05-02 14:48:23 +0800

    Initial commit

---
+++ src/main/java/com/example/csharing/CSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.csharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/csharing/CSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
